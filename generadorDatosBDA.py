5a07bf8cee1f360bdadf91688141fc9bf0389579
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2026-02-10 12:57:10 +0100

    commit python

--- generadorDatosBDA.py
+++ generadorDatosBDA.py
@@ -1,10 +1,12 @@
 import random
 import os
-print("Directorio de trabajo:", os.getcwd())
+#print("Directorio de trabajo:", os.getcwd())
+
+random.seed(1234)
 
 NUM_REGISTROS = 30000000
 
-with open("estudiantes.csv", "w", encoding="utf-8") as f:
+with open("C:/Users/lucas/Desktop/BDA/estudiantes.csv", "w", encoding="utf-8") as f:
     for i in range(NUM_REGISTROS):
         nombre = f"Estudiante_{i}"
         codigo_carrera = random.randint(0, 100)
@@ -12,4 +14,4 @@ with open("estudiantes.csv", "w", encoding="utf-8") as f:
         indice = random.randint(0, 10000)
 
         f.write(f"{nombre},{codigo_carrera},{edad},{indice}\n")
-
+    print("archivo creado/actualizado")
